88565fe0d663211c458db5030459f2ee00ea1360
It-is-my-work <itsmywork2018@yandex.ru>
2020-11-29 23:24:59 +0300

    Добавлен пример теста

---
+++ new_selenium_project/test_example.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@pytest.fixture
+def driver(request):
+    browser = webdriver.Firefox()
+    request.addfinalizer(browser.quit)
+    return browser
+
+def test_example(driver):
+    driver.get("http://google.com")
+    driver.find_element_by_name("q").send_keys("webdriver")
+    driver.find_element_by_name("btnK").click()
+    WebDriverWait(driver, 10).until(EC.title_is("webdriver - Поиск в Google"))
